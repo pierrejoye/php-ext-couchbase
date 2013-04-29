6d1c4577c01bb1193761845c6b5f9f7165e39623
Pierre Joye <pierre.php@gmail.com>
2013-04-29 19:10:39 +0200

    add version and WS fix

--- php_couchbase.h
+++ php_couchbase.h
@@ -1,24 +1,26 @@
 /*
   +----------------------------------------------------------------------+
-  | PHP Version 5														 |
+  | PHP Version 5							 |
   +----------------------------------------------------------------------+
-  | Copyright 2012 Couchbase, Inc.										 |
+  | Copyright 2012 Couchbase, Inc.					 |
   +----------------------------------------------------------------------+
-  | Licensed under the Apache License, Version 2.0 (the "License");		 |
+  | Licensed under the Apache License, Version 2.0 (the "License");	 |
   | you may not use this file except in compliance with the License.	 |
-  | You may obtain a copy of the License at								 |
-  |		http://www.apache.org/licenses/LICENSE-2.0						 |
+  | You may obtain a copy of the License at				 |
+  |		http://www.apache.org/licenses/LICENSE-2.0		 |
   | Unless required by applicable law or agreed to in writing, software	 |
   | distributed under the License is distributed on an "AS IS" BASIS,	 |
-  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or		 |
-  | implied. See the License for the specific language governing		 |
-  | permissions and limitations under the License.						 |
-  +----------------------------------------------------------------------+
-  | Author: Xinchen Hui	   <laruence@php.net>							 |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or	 |
+  | implied. See the License for the specific language governing	 |
+  | permissions and limitations under the License.			 |
+  +------------------------------------------------------------- --------+
+  | Author: Xinchen Hui	   <laruence@php.net>				 |
   +----------------------------------------------------------------------+
 */
 #ifndef PHP_COUCHBASE_H
 #define PHP_COUCHBASE_H
+
+#define PHP_COUCHBASE_VERSION_STRING "1.1.5-dev"
 
 extern zend_module_entry couchbase_module_entry;
 #define phpext_couchbase_ptr &couchbase_module_entry
